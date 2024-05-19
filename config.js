0c3bc52690ecad5de9c887e4b7d5f0819833a624
Donna310 <mythach03102003@gmail.com>
2024-05-19 15:37:51 +0700

    Add files via upload

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
-const nameGirl = 'nguyễn văn ';
+const nameGirl = 'PÔN ';
 const giftUrl = 'http://facebook.com';
-const eventName = 'Gửi A ';
-const titleCard = 'Tặng a';
-const contentCard = 'A ơi, gặp được chị trong thế giới này là một điều may mắn đối với em vì thế giới này rộng lắm, gặp được chị, nếu có thể chị là một phần trong cuộc sống của em nhé .';
+const eventName = 'Gửi Anh ';
+const titleCard = 'Tặng anh iu';
+const contentCard = 'Anh yêu ơi, sinh nhật vui vẻ! Cảm ơn anh vì mọi điều tốt đẹp anh đã mang đến cho cuộc đời em. Mong rằng ngày sinh nhật của anh sẽ đầy niềm vui và hạnh phúc, như anh vẫn mang lại cho em .Em biết rằng tình yêu của chúng ta sẽ cùng trải qua mọi thăng trầm, và em thật sự cảm kích vì sự hiện diện quan trọng của anh trong cuộc đời em.Đặc biệt hơn hết là em mong rằng tất cả mơ ước của anh sẽ thành hiện thực. Em iu anh nhiều lắm ạ.';
 // phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
 // Cần hỗ trợ hãy liên hệ: 
 // Mr-Nam http://facebook.com/nam.nodemy
